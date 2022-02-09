8f6e8a74907e0f6e575d9be936835f9622cbf2f6
ReiSikk <reix0010@stud.kea.dk>
2022-02-09 17:36:35 +0100

    made productpage info dynamic

--- product.js
+++ product.js
@@ -19,4 +19,16 @@ function showProduct(product) {
     "img.productimage"
   ).src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
   document.querySelector("img.productimage").alt = product.productdisplayname;
+  document.querySelector(".purchaseBox .productname").textContent =
+    product.productdisplayname;
+  document.querySelector(".purchaseBox .brandname").textContent =
+    product.brandname;
+  document.querySelector(".purchaseBox .articletype").textContent =
+    product.articletype;
+  document.querySelector("#info .productname").textContent =
+    product.productdisplayname;
+  document.querySelector(".color").textContent = product.basecolour;
+  document.querySelector(".inventorynumber").textContent = product.id;
+  document.querySelector("#info .brandname").textContent = product.brandname;
+  document.querySelector("#info .brandbio").textContent = product.brandbio;
 }
